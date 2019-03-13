7cc785879df70a478cff67b0a245470c18941f9b
przemekkniola1 <kaka060@wp.pl>
2019-03-13 10:11:13 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine());
+
+    }
+}
